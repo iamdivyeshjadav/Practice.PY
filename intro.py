c682773189ba6104401573c8a4f75b82cc93e84a
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-05-05 18:15:01 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,11 @@
+print("my name is jadav divyesh")
+print("my age is 17")
+print("my hobbies are coding and playing cricket")
+print("my favorite color is blue")
+print("my favorite food is .......")
+print("my favorite movie is avengers endgame")
+print("i study in red and white institution")
+a=5;
+b=10;
+c=a+b;
+print("the sum of a and b is:", c)
