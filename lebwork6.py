0564cf1ebbd836cc74873b5e1e6e925697aefddf
iamdivyeshjadav <divyeshjadav2008@gmail.com>
2026-05-15 16:32:07 +0530

    Updated with 7 and 8

---
+++ lebwork6.py
@@ -0,0 +1,17 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+if a>b:
+   print("a is largest number") 
+elif b>c:
+    print("b is largest number")
+elif c>a:
+    print("c is largest number")
+else:
+    print("Please enter valid numbers.")
+'''output:
+Enter first number: 10
+Enter second number: 20
+Enter third number: 30
+c is largest number
+'''
---
+++ lebwork7.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number: "))
+if n==0: 
+    print("The number is Neutral")
+else:
+    print("The number is not Neutral")
+'''output:
+Enter a number: 0
+The number is Neutral
+'''
+
+
